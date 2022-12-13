316a79ea2f6ccbb35960e635aee12f909c58b8c2
Umamageshwaran <umamageshwaran.r@socure.com>
2022-12-13 14:26:33 +0530

    3.1.0

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SocureDocV",
-            url: "https://sdk.socure.com/socure-sdks/docv/ios/socure-docv-3.0.2.zip",
-            checksum: "52e3461b35061a10851045be4bb525b4187d9c624a6e69968a8069f0527f2682"
+            url: "https://sdk.socure.com/socure-sdks/docv/ios/socure-docv-3.1.0.zip",
+            checksum: "ddb0316a834234e41093657753a9e4f8cac6353f44c88897f27c239f31df51cd"
         )
     ]
 )
