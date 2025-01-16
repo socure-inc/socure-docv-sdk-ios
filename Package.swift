51cab0673f6b16e52d79d4c82ebeed635ab9d73e
Boris Mezhibovskiy <boris.mezhibovskiy@socure.com>
2025-01-16 11:31:07 -0600

    v5.0.7

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SocureDocV",
-            url: "https://sdk.socure.com/socure-sdks/docv/ios/socure-docv-5.0.6.zip",
-            checksum: "2924f330c41d2bfa52fd1024de7e9b40118612497d60c02bce5d7fc16a3ce0df"
+            url: "https://sdk.socure.com/socure-sdks/docv/ios/socure-docv-5.0.7.zip",
+            checksum: "d5e9008cd48e45ba090e4b4373093c248019ffaff9b23b0efaea8b2f67a47641"
         )
     ]
 )
