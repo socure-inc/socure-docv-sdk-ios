da9e9391410e3a28e41a84586aae572c8f1da820
Boris Mezhibovskiy <boris.mezhibovskiy@socure.com>
2025-04-09 13:03:26 -0500

    v5.1.1

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
             targets: ["SocureDocV"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/socure-inc/socure-sigmadevice-sdk-ios", from: "4.5.2")
+        .package(url: "https://github.com/socure-inc/socure-sigmadevice-sdk-ios", "4.5.2"..<"4.6.0")
     ],
     targets: [
         .binaryTarget(
             name: "SocureDocV",
-            url: "https://sdk.socure.com/socure-sdks/docv/ios/socure-docv-5.1.0.zip",
-            checksum: "09f111a475fa8fbaefc14ecc7209a69568d8d704f75e47b5d936e0fea348a8bf"
+            url: "https://sdk.socure.com/socure-sdks/docv/ios/socure-docv-5.1.1.zip",
+            checksum: "cf4336836e86f06b6cd1d76270d301b9f1bd5c30b8d1c500cb2b298fed10f502"
         )
     ]
 )
