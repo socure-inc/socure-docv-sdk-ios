e8dd7125a261baf0b730a20629333e05614978ae
Boris Mezhibovskiy <boris.mezhibovskiy@socure.com>
2025-08-19 15:55:52 -0500

    v5.2.5

--- Package.swift
+++ Package.swift
@@ -3,20 +3,34 @@
 import PackageDescription
 let package = Package(
     name: "SocureDocV",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SocureDocV",
-            targets: ["SocureDocV"]),
+            targets: ["SocureDocV"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/socure-inc/socure-sigmadevice-sdk-ios", "4.5.2"..<"4.6.0")
+        .package(
+            url: "https://github.com/socure-inc/socure-sigmadevice-sdk-ios", "4.5.2"..<"4.6.0"
+        )
     ],
     targets: [
         .binaryTarget(
+            name: "SocureDocVBinary",
+            url: "https://sdk.socure.com/socure-sdks/docv/ios/socure-docv-5.2.5.zip",
+            checksum: "a6cb1064e467a4ac75eabaf7600bc549e4dbed7eb0519c00ac25db67b1050c4e"
+        ),
+        .target(
             name: "SocureDocV",
-            url: "https://sdk.socure.com/socure-sdks/docv/ios/socure-docv-5.2.0.zip",
-            checksum: "2ac0034c24bb8aa193321e53c5268315d356c3a250f3836f043759dc2f7e4629"
+            dependencies: [
+                "SocureDocVBinary",
+                .product(name: "DeviceRisk", package: "socure-sigmadevice-sdk-ios")
+            ],
+            path: "Sources/SocureDocV",
+            publicHeadersPath: "."
         )
     ]
 )
---
+++ Sources/SocureDocV/SocureDocV.swift
@@ -0,0 +1,2 @@
+@_exported import SocureDocVBinary
+@_exported import DeviceRisk
