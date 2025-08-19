a815210951ba068f65fd9fe058dacedf6ad27dcb
Boris Mezhibovskiy <boris.mezhibovskiy@socure.com>
2025-08-19 16:44:25 -0500

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -9,24 +9,25 @@ let package = Package(
     products: [
         .library(
             name: "SocureDocV",
-            targets: ["SocureDocV"]
+            targets: ["SocureDocVWrapper"]
         )
     ],
     dependencies: [
         .package(
-            url: "https://github.com/socure-inc/socure-sigmadevice-sdk-ios", "4.5.2"..<"4.6.0"
+            url: "https://github.com/socure-inc/socure-sigmadevice-sdk-ios",
+            "4.5.2"..<"4.6.0"
         )
     ],
     targets: [
         .binaryTarget(
-            name: "SocureDocVBinary",
+            name: "SocureDocV",
             url: "https://sdk.socure.com/socure-sdks/docv/ios/socure-docv-5.2.5.zip",
             checksum: "a6cb1064e467a4ac75eabaf7600bc549e4dbed7eb0519c00ac25db67b1050c4e"
         ),
         .target(
-            name: "SocureDocV",
+            name: "SocureDocVWrapper",
             dependencies: [
-                "SocureDocVBinary",
+                "SocureDocV",
                 .product(name: "DeviceRisk", package: "socure-sigmadevice-sdk-ios")
             ],
             path: "Sources/SocureDocV",
